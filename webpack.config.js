4cf8da3625f9ab5a4c73c75e515d981eacfe79b4
freewind <nowindlee@gmail.com>
2019-01-11 21:29:11 +0800

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
 module.exports = {
+  mode: 'development',
   entry: './entry.jsx',
   output: {
     path: __dirname,
-    publicPath: '/assets/',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
+    rules: [{
       test: /\.(js|jsx)$/,
       exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react']
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react'
+          ]
+        }
       }
     }]
   }
